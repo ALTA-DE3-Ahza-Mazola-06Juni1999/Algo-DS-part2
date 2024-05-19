3346e2fa5564cc4bebb65c00377e3e16b285fe13
Ahza Mazola <ahzamazola@gmail.com>
2024-05-19 19:05:06 +0700

    add answer

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,11 @@
 def playing_domino(cards, deck):
-    return []
+    valid_cards =[card for card in cards if deck[0] in card or deck[1] in card]
+
+    if not valid_cards:
+        return []
+    
+    return max(valid_cards, key=lambda card: sum(card))
+
 
 if __name__ == "__main__":
     print(playing_domino([[6, 5], [3, 4], [2, 1], [3, 3]], [4, 3]))  # [3, 4]
